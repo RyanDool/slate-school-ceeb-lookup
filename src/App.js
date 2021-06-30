a61c6aa918145e0922aa3c903034efd2a3f538df
Ryan Dool <ryandool@gmail.com>
2021-06-30 16:09:03 -0700

    CORS permissions issues with slate fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		fetch("https://slate.admissions.arizona.edu/manage/query/run?id=e792c47b-49af-4813-aa61-b4ae74a20928&cmd=service&output=json")
+		.then((response) => response.json())
+		.then((data) => console.log(data))
+	}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<h1>Fetching Slate</h1>
     </div>
   );
 }
